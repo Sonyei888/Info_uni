a7fa1eba23284295c0be74f2933fd24755f427a1
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2022-10-20 19:34:09 -0500

    Se crea una nueva actividad

---
+++ app/src/main/java/com/example/uni_info/AdminVerNoticias.java
@@ -0,0 +1,14 @@
+package com.example.uni_info;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminVerNoticias extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_ver_noticias);
+    }
+}
