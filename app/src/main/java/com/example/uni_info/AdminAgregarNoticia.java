07af0693614501be7ab9e4c323ced2ca1481260b
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2023-05-13 12:58:05 -0500

    Se agrego un seleccionador de fecha al agregar una noticia

--- app/src/main/java/com/example/uni_info/AdminAgregarNoticia.java
+++ app/src/main/java/com/example/uni_info/AdminAgregarNoticia.java
@@ -2,13 +2,17 @@ package com.example.uni_info;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.uni_info.BD.dbInfo;
@@ -18,16 +22,20 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Calendar;
+import java.util.Locale;
 import java.util.UUID;
 
 public class AdminAgregarNoticia extends AppCompatActivity implements View.OnClickListener {
     EditText titulo;
     EditText resumen;
-    EditText fecha;
-    EditText hora;
+    private TextView tvFecha;
+    private Button btnSeleccionarFecha;
+    private Calendar calendar;
     Metodos metodos;
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
+    private String fecha;
 
 
     @Override
@@ -36,11 +44,12 @@ public class AdminAgregarNoticia extends AppCompatActivity implements View.OnCli
         setContentView(R.layout.activity_admin_agregar_noticia);
         titulo = findViewById(R.id.edit_titulo_noticia);
         resumen = findViewById(R.id.edit_resumen_noticia);
+        tvFecha = findViewById(R.id.tv_fecha);
+        calendar = Calendar.getInstance();
 /*      informacion = findViewById(R.id.edit_info_noticia);*/
-        fecha = findViewById(R.id.edit_fecha_noticia);
-        hora = findViewById(R.id.edit_hora_noticia);
         findViewById(R.id.btn_agregar_noticia).setOnClickListener(this);
         findViewById(R.id.btn_cancelar_agregar_noticia).setOnClickListener(this);
+        findViewById(R.id.btn_seleccionar_fecha).setOnClickListener(this);
         metodos = new Metodos(this);
         Noticias noticias = new Noticias();
         inicializarFirebase();
@@ -55,6 +64,33 @@ public class AdminAgregarNoticia extends AppCompatActivity implements View.OnCli
     @Override
     public void onClick(View view) {
         switch (view.getId()){
+            case R.id.btn_seleccionar_fecha:
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        AdminAgregarNoticia.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                // Actualizar el TextView con la fecha seleccionada
+                                fecha = String.format(Locale.getDefault(), "%02d/%02d/%d", dayOfMonth, month + 1, year);
+                                tvFecha.setText(fecha);
+
+
+                            }
+                        },
+                        year, month, dayOfMonth);
+
+                // Establecer la fecha mínima seleccionable en un día después de la fecha actual
+                calendar.add(Calendar.DAY_OF_MONTH, 1);
+                datePickerDialog.getDatePicker().setMinDate(calendar.getTimeInMillis());
+                calendar.add(Calendar.DAY_OF_MONTH, -1);
+
+                datePickerDialog.show();
+
+                break;
             case R.id.btn_agregar_noticia:
                 //se declara e instancia la clase noticias
                 Noticias noticias = new Noticias();
@@ -62,12 +98,12 @@ public class AdminAgregarNoticia extends AppCompatActivity implements View.OnCli
                 noticias.setId(UUID.randomUUID().toString());
                 noticias.setNombre(titulo.getText().toString());
                 noticias.setResumen(resumen.getText().toString());
-                noticias.setFecha(fecha.getText().toString());
-                noticias.setHora(hora.getText().toString());
+                noticias.setFecha(fecha);
+                /*noticias.setHora(hora.getText().toString());*/
                 noticias.setDatabase("Bases de datos online");
 
                 //se comprueba que los campos no esten vacios
-                if(!noticias.getNombre().isEmpty() && !noticias.getResumen().isEmpty() && !noticias.getFecha().isEmpty() && !noticias.getHora().isEmpty()){
+                if(!noticias.getNombre().isEmpty() && !noticias.getResumen().isEmpty() && !noticias.getFecha().isEmpty() /*&& !noticias.getHora().isEmpty()*/){
                    //si es verdad se agrega la noticia a la base de datos
                     databaseReference.child("Noticias").child(noticias.getId()).setValue(noticias);
                     metodos.insertnoticia(noticias); //se invoca el metodo insertar noticia, el cual inserta la noticia en una base de datos local
@@ -90,12 +126,13 @@ public class AdminAgregarNoticia extends AppCompatActivity implements View.OnCli
                 finish();
                 break;
         }
+
     }
     private void validacion(){
         String nombre = titulo.getText().toString();
         String resumen1 = resumen.getText().toString();
-        String fecha1 = fecha.getText().toString();
-        String hora1 = hora.getText().toString();
+        String fecha1 = fecha;
+        /*String hora1 = hora.getText().toString();*/
         if(nombre.isEmpty()){
             titulo.setError("Requerido");
         }
@@ -103,11 +140,11 @@ public class AdminAgregarNoticia extends AppCompatActivity implements View.OnCli
             resumen.setError("Requerido");
         }
         if(fecha1.isEmpty()){
-            fecha.setError("Requerido");
+            tvFecha.setError("Requerido");
         }
-        if(hora1.isEmpty()){
+        /*if(hora1.isEmpty()){
             hora.setError("Requerido");
-        }
+        }*/
     }
 
 }
